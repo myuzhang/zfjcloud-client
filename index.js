0574bbfcd8995a2c33707e5a1a1ee92787e9adf5
Michael ZHANG <yuyu.zh2012@gmail.com>
2019-03-18 18:24:37 +1100

    Upload business file

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-const Jira = require('./jiraService');
-const Zephyr = require('./zephyrService');
+'use strict';
 
-module.exports = class ZfjCloud {
-    constructor(settings) {
-        this.jira = new Jira(settings.jira.auth, settings.projectName);
-        this.zephyr = new Zephyr(settings.zephyr.accessKey, settings.zephyr.secretKey, settings.zephyr.user);
-    }
-
-    // Test status:
-    // Inconclusive = -1,
-    // Passed = 1,
-    // Failed = 2,
-    // InProgress = 3
-    updateExecutionResult(versionName, testCaseName, testResult) {
-        let issue = this.jira.getIssue(testCaseName);
-
-        let version = this.jira.getVersion(versionName);
-
-        let executions = this.zephyr.getAllExecutionsByIssueInVersion(issue.id, version.id, this.jira.project.id);
-        if(executions.length > 0) {
-            for(let i = 0; i < executions.length; i++) {
-                let execution = executions[i].execution;
-                this.zephyr.updateExecution(execution.id, execution.cycleId, execution.projectId, version.id, issue.id, testResult);
-            }
-        }
-    }
-}
+module.exports = require('./zfjcould.js');
---
+++ zfjcloud.js
@@ -0,0 +1,28 @@
+const Jira = require('./jiraService');
+const Zephyr = require('./zephyrService');
+
+module.exports = class ZfjCloud {
+    constructor(settings) {
+        this.jira = new Jira(settings.jira.auth, settings.projectName);
+        this.zephyr = new Zephyr(settings.zephyr.accessKey, settings.zephyr.secretKey, settings.zephyr.user);
+    }
+
+    // Test status:
+    // Inconclusive = -1,
+    // Passed = 1,
+    // Failed = 2,
+    // InProgress = 3
+    updateExecutionResult(versionName, testCaseName, testResult) {
+        let issue = this.jira.getIssue(testCaseName);
+
+        let version = this.jira.getVersion(versionName);
+
+        let executions = this.zephyr.getAllExecutionsByIssueInVersion(issue.id, version.id, this.jira.project.id);
+        if(executions.length > 0) {
+            for(let i = 0; i < executions.length; i++) {
+                let execution = executions[i].execution;
+                this.zephyr.updateExecution(execution.id, execution.cycleId, execution.projectId, version.id, issue.id, testResult);
+            }
+        }
+    }
+}
